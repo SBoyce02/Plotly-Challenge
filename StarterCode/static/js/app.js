0bf7e5aa3dee4d1cbf21667c7ecec0394bddb89e
SBoyce02 <sroecker1983@gmail.com>
2021-04-28 21:40:32 -0500

    json data console logged

---
+++ StarterCode/static/js/app.js
@@ -0,0 +1,13 @@
+
+
+// Fetch the JSON data and console log it
+ d3.json("samples.json").then(function (data) {
+    console.log(data);
+  });
+  
+
+  // add variable
+
+//   const BBdata =  d3.json("samples.json");
+//   console.log("Belly Button Data": BBdata );
+  
