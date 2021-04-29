34b3891f6376012d6bf98faf8547f00d2d51f3f0
SBoyce02 <sroecker1983@gmail.com>
2021-04-28 23:26:49 -0500

    2nd attempt at unpack

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -6,32 +6,34 @@
   });
   
   
-function unpack(rows, index) {
-    return rows.map(function(row) {
-      return row[index];
-    });
-  }
-  
+// function unpack(rows, index) {
+//     return rows.map(function(row) {
+//       return row[index];
+//     });
+//   }
   
 
-
-function buildPlot() {
+// function buildPlot() {
     d3.json("samples.json").then(data => {
-        console.log(data.metadata);
-        console.log(data.metadata.samples);
+        console.log(data.samples);
+     
+        var otu_id =  data.samples.otu_ids;
+        var otu_label = data.samples.otu_labels;
+        var otu_values = data.samples.sample_values;
+        console.log(otu_label);
 
-        var trace1 = {
-         type: 'bar'
-         x: []
-         y:[]
-         text: []
-         name: Top 10 OTUs  
-        }
+    }
+//         var trace1 = {
+//          type: 'bar',
+//          x: unpack(data.metadata.samples, 0),
+//          y: unpack(data.metadata.samples, 3),
+//         //  text: []
+//          name: 'Top 10 OTUs',  
+//         }
 
+//         var traceData = [trace1];
+//         Plotly.plot('#bar', tracedata)
+//     });
+// }
 
-
-
-    });
-}
-
-buildPlot();
+// buildPlot();
